5305c0c3bc7903522a2e5703f18f7793018ffaca
ano <fatihsefer502@gmail.com>
2025-11-03 11:27:49 +0700

    add navbar components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar">
+                <a href="#"><p>BERANDA</p></a>
+                <a href="#"><p>TENTANG KAMI</p></a>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
