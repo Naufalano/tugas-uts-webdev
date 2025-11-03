3980f4351fa5e87348d62a95a5cdc0932d0b1e9e
ano <fatihsefer502@gmail.com>
2025-11-03 11:26:23 +0700

    reconfigure body size

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -5,8 +5,9 @@ function Body() {
     return (
         <>
             <div className="body">
-                <p>Produsen veneer kayu berkualitas. Siap melayani segala jenis ukuran dan jenis kayu.</p>
-                <p>Silakan kontak narahubung kami atau lihat produk yang kami tawarkan.</p>
+                <div className="mainbody">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eveniet magnam ea nisi rerum? Aperiam, iure! Atque non eos, facere ut rerum temporibus eligendi sit optio aliquam nesciunt, error ipsam.</p>
+                </div>
             </div>
         </>
     )
