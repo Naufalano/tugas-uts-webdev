f5b4a56a704b5d458924e29b90bb2e0c0ba26643
ano <fatihsefer502@gmail.com>
2025-11-03 11:28:14 +0700

    add navbar to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Body from './components/Body'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
       <Header />
+      <Navbar />
       <Body />
       <Footer />
     </>
